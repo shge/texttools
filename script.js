9d071a10e0dbb9eef7a2cd6a11039a87bce12e2e
shge <25113178+shge@users.noreply.github.com>
2018-04-08 00:53:19 +0900

    Add tabs

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(function() {
 
+// Header
 $('#nav').load('../nav.html?201804062022')
+
+// Change tabs
+$('.tabs li').on('click', function() {
+  $('.tabs li').removeClass('is-active');
+  $(this).addClass('is-active');
+  $('#tab-contents > div').addClass('hidden');
+  $('#tab-contents > div').eq( $('.tabs li').index(this) ).removeClass('hidden')
+});
+
+
+
 
 });
